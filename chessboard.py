59d357a204d1ddc2f4a006be0d2f8e8df4e1b47f
Just_A_Pi <107017897+asdy31415@users.noreply.github.com>
2022-12-14 16:03:54 +0800

    Update chessboard.py

--- chessboard.py
+++ chessboard.py
@@ -1,1 +1,20 @@
+import pygmae
 
+import time
+
+import sis
+
+board[[0,0,0,0,0,0,0,0]
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0,0,0,0]]
+
+class Piece:
+    def __init__(self, team, type, image, killable):
+        self.team = team
+        self.type = type
+        self.image = image
